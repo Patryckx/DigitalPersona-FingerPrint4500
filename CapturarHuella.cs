cd446b0ae66975ddcf84a13b4522470ebd517659
Patryck <winonacsx@gmail.com>
2024-07-25 22:14:14 -0600

    Compiled app no database implementation

--- CapturarHuella.cs
+++ CapturarHuella.cs
@@ -10,13 +10,66 @@ using System.Windows.Forms;
 
 namespace DititalPerson4500
 {
-    public partial class CapturarHuella : Form
+    public partial class CapturarHuella : CaptureForm
     {
         public delegate void OnTemplateEventHandler(DPFP.Template template);
 
         public event OnTemplateEventHandler OnTemplate;
 
         private DPFP.Processing.Enrollment Enroller;
+
+        protected override void Init()
+        {
+            base.Init();
+            base.Text = "Dar de alta Huella";
+            Enroller = new DPFP.Processing.Enrollment();            // Create an enrollment.
+            UpdateStatus();
+        }
+
+        private void UpdateStatus()
+        {
+            // Show number of samples needed.
+            SetStatus(String.Format("Fingerprint samples needed: {0}", Enroller.FeaturesNeeded));
+        }
+
+        protected override void Process(DPFP.Sample Sample)
+        {
+            base.Process(Sample);
+
+            // Process the sample and create a feature set for the enrollment purpose.
+            DPFP.FeatureSet features = ExtractFeatures(Sample, DPFP.Processing.DataPurpose.Enrollment);
+
+            // Check quality of the sample and add to enroller if it's good
+            if (features != null) try
+                {
+                    MakeReport("The fingerprint feature set was created1.");
+                    Enroller.AddFeatures(features);     // Add feature set to template.
+                }
+                finally
+                {
+                    UpdateStatus();
+
+                    // Check if template has been created.
+                    switch (Enroller.TemplateStatus)
+                    {
+                        case DPFP.Processing.Enrollment.Status.Ready:   // report success and stop capturing
+                            OnTemplate(Enroller.Template);
+                            SetPrompt("Click Close, and then click Fingerprint Verification.");
+                            Stop();
+                            break;
+
+                        case DPFP.Processing.Enrollment.Status.Failed:  // report failure and restart capturing
+                            Enroller.Clear();
+                            Stop();
+                            UpdateStatus();
+                            OnTemplate(null);
+                            Start();
+                            break;
+                    }
+                }
+        }
+
+
         public CapturarHuella()
         {
             InitializeComponent();
@@ -25,6 +78,7 @@ namespace DititalPerson4500
         private void CapturarHuella_Load(object sender, EventArgs e)
         {
 
-        }
+        
     }
+}
 }
